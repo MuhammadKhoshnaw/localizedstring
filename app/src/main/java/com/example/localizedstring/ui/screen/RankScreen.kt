2d7bf584e3e48cdb3f72f27f29089e6432432d10
MuhammadXoshnaw <muhammad.xoshnaw.eng@gmail.com>
2024-04-21 06:39:16 +0300

    Improving the UI

--- app/src/main/java/com/example/localizedstring/ui/screen/RankScreen.kt
+++ app/src/main/java/com/example/localizedstring/ui/screen/RankScreen.kt
@@ -1,27 +1,46 @@
 package com.example.localizedstring.ui.screen
 
+import android.widget.Space
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.AnimationConstants.DefaultDurationMillis
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,8 +51,11 @@ import com.example.localizedstring.R
 import com.example.localizedstring.entity.LocalizedString
 import com.example.localizedstring.entity.localizedRowString
 import com.example.localizedstring.ui.theme.LocalizedStringTheme
+import com.example.localizedstring.viewModel.RankActions
 import com.example.localizedstring.viewModel.RankViewModel
 import com.example.localizedstring.viewModel.string
+import kotlinx.coroutines.delay
+import kotlin.math.abs
 
 @Composable
 fun RankScreen(
@@ -45,6 +67,7 @@ fun RankScreen(
         title = uiState.value.title,
         body = uiState.value.body,
         avatar = uiState.value.avatar,
+        rankActions = uiState.value.actions,
         onCountClicked = viewModel::onCountClicked,
     )
 }
@@ -54,6 +77,7 @@ fun RankScreen(
     title: LocalizedString,
     body: LocalizedString,
     @DrawableRes avatar: Int?,
+    rankActions: RankActions,
     onCountClicked: () -> Unit,
 ) {
     Scaffold { innerPadding ->
@@ -69,80 +93,227 @@ fun RankScreen(
                     .fillMaxSize(),
             ) {
 
-                Crossfade(
+                Avatar(
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(2f),
-                    targetState = avatar,
-                    label = "rank avatar",
-                    animationSpec = tween(
-                        durationMillis = DefaultDurationMillis,
-                        easing = LinearEasing
-                    ),
-                ) { avatar ->
-                    AvatarImage(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        painter = avatar?.let {
-                            painterResource(id = it)
-                        } ?: ColorPainter(Color.Transparent),
-                    )
-                }
+                    avatar = avatar,
+                )
 
-                Column(
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(1f)
-                        .padding(horizontal = 32.dp),
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
-                ) {
+                        .weight(weight = 1f)
+                )
 
-                    Crossfade(
-                        targetState = title.string(),
-                        label = "rank title"
-                    ) {
-                        Text(
-                            text = it,
-                            style = MaterialTheme.typography.titleLarge,
-                            modifier = Modifier.padding(vertical = 8.dp)
-                        )
-                    }
-
-
-                    Text(
-                        text = body.string(),
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    )
-
-                    FloatingActionButton(
-                        onClick = onCountClicked,
-                        modifier = Modifier
-                            .padding(top = 24.dp)
-                            .align(alignment = Alignment.CenterHorizontally),
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = Icons.Default.Done.name,
-                        )
-                    }
-
-                }
             }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1.2f),
+                )
+
+                Actions(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(weight = 1f, fill = true)
+                        .padding(horizontal = 16.dp),
+                    title = title,
+                    body = body,
+                    rankActions = rankActions,
+                    onCountClicked = onCountClicked,
+                )
+            }
+        }
+
+        BoxWithConstraints {
+
         }
     }
 }
 
+@Composable
+private fun Avatar(
+    modifier: Modifier = Modifier,
+    @DrawableRes avatar: Int?,
+) {
+    Crossfade(
+        modifier = modifier,
+        targetState = avatar,
+        label = "rank avatar",
+        animationSpec = tween(
+            durationMillis = DefaultDurationMillis,
+            easing = LinearEasing
+        ),
+    ) { currentAvatar ->
+        AvatarImage(
+            modifier = Modifier
+                .fillMaxSize(),
+            painter = currentAvatar?.let {
+                painterResource(id = it)
+            } ?: ColorPainter(Color.Transparent),
+        )
+    }
+}
+
+@Composable
+private fun Actions(
+    modifier: Modifier = Modifier,
+    title: LocalizedString,
+    body: LocalizedString,
+    rankActions: RankActions,
+    onCountClicked: () -> Unit,
+) {
+    val topFade = Brush.verticalGradient(
+        0f to Color.Transparent,
+        0.3f to Color.Black,
+    )
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .graphicsLayer { alpha = 0.99F }
+            .drawWithContent {
+                val colors = listOf(
+                    Color.Transparent,
+                    Color.Black,
+                    Color.Black,
+                    Color.Black,
+                    Color.Black,
+                    Color.Black,
+                    Color.Black,
+                    Color.Black,
+                )
+                drawContent()
+                drawRect(
+                    brush = Brush.verticalGradient(
+                        0f to Color.Transparent,
+                        0f to Color.Black,
+                    ),
+                    blendMode = BlendMode.DstIn
+                )
+            },
+
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Bottom
+    ) {
+
+        Spacer(modifier = Modifier.padding(vertical = 32.dp))
+
+        Crossfade(
+            targetState = title.string(),
+            label = "rank title"
+        ) {
+            Text(
+                text = it,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+
+
+        Text(
+            text = body.string(),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+
+
+        AnimatedContent(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally),
+            targetState = rankActions,
+            label = "rankActions",
+        ) { action ->
+            when (action) {
+                RankActions.COUNT_CLICKS -> FloatingActionButton(
+                    onClick = onCountClicked,
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = Icons.Default.Done.name,
+                    )
+                }
+
+                RankActions.UNIVERSE_DECISION -> Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                ) {
+                    var buttonsEnabled by remember(action) {
+                        mutableStateOf(false)
+                    }
+                    var countDown by remember(action) {
+                        mutableIntStateOf(3)
+                    }
+
+                    LaunchedEffect(key1 = action) {
+                        repeat(3) { count ->
+                            countDown = abs(3 - count)
+                            delay(1000L)
+                        }
+                        buttonsEnabled = !buttonsEnabled
+                    }
+
+                    OutlinedButton(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        enabled = buttonsEnabled,
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Text(
+                            text = if (buttonsEnabled) "Reset Existence"
+                            else "Reset Existence in $countDown",
+                        )
+                    }
+
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        enabled = buttonsEnabled,
+                        onClick = { /*TODO*/ }) {
+                        Text(
+                            text = if (buttonsEnabled) "Forge a New Reality"
+                            else "Forge a New Reality in $countDown",
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(vertical = 24.dp))
+    }
+}
+
 @Preview(showBackground = true)
-@Preview(showBackground = true, device = "id:Nexus 10")
+//@Preview(showBackground = true, device = "id:Nexus One")
 @Composable
 private fun Preview() {
     LocalizedStringTheme {
         RankScreen(
             title = localizedRowString("Current Level : Apprentice \uD83D\uDC76"),
-            body = localizedRowString("Click to increase your level!, you have clicked 0 times."),
+            body = localizedRowString(
+                "Click to increase your level!, you have clicked 0 times." +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!" +
+                        " You are currently an Apprentice. Click to become a Master!"
+
+            ),
             avatar = R.drawable.rank_0,
+            rankActions = RankActions.COUNT_CLICKS,
             onCountClicked = { }
         )
     }
--- app/src/main/java/com/example/localizedstring/viewModel/RankViewModel.kt
+++ app/src/main/java/com/example/localizedstring/viewModel/RankViewModel.kt
@@ -8,6 +8,7 @@ import com.example.localizedstring.entity.empty
 import com.example.localizedstring.entity.localizedString
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import timber.log.Timber
 
 class RankViewModel : ViewModel() {
@@ -33,79 +34,87 @@ class RankViewModel : ViewModel() {
     private fun updateRank() {
         Timber.i("updating rank")
 
-        val (title, body, avatar) = when (count) {
-            in RANK_0_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_0)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_0
+        val newUIState: WelcomeUIState = when (count) {
+            in RANK_0_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_0)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_0,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_1_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_1)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_1
+            in RANK_1_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_1)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_1,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_2_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_2)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_2
+            in RANK_2_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_2)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_2,
+                actions = RankActions.COUNT_CLICKS,
+
+                )
+
+            in RANK_3_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_3)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_3,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_3_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_3)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_3
+            in RANK_4_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_4)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_4,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_4_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_4)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_4
+            in RANK_5_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_5)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_5,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_5_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_5)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_5
+            in RANK_6_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_6)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_6,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_6_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_6)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_6
+            in RANK_7_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_7)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_7,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_7_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_7)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_7
+            in RANK_8_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_8)),
+                body = localizedString(R.string.rank_body, count.toString()),
+                avatar = R.drawable.rank_8,
+                actions = RankActions.COUNT_CLICKS,
             )
 
-            in RANK_8_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_8)),
-                localizedString(R.string.rank_body, count.toString()),
-                R.drawable.rank_8
+            in RANK_9_RANGE -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, localizedString(R.string.rank_9)),
+                body = localizedString(R.string.last_rank_body, count.toString()),
+                avatar = R.drawable.rank_9,
+                actions = RankActions.UNIVERSE_DECISION,
             )
 
-            in RANK_9_RANGE -> Triple(
-                localizedString(R.string.rank_title, localizedString(R.string.rank_9)),
-                localizedString(R.string.last_rank_body, count.toString()),
-                R.drawable.rank_9
-            )
-
-            else -> Triple(
-                localizedString(R.string.rank_title, String.empty),
-                localizedString(R.string.rank_body, String.empty),
-                null,
+            else -> WelcomeUIState(
+                title = localizedString(R.string.rank_title, String.empty),
+                body = localizedString(R.string.rank_body, String.empty),
+                avatar = null,
+                actions = RankActions.COUNT_CLICKS,
             )
         }
 
-        _uiState.value = WelcomeUIState(
-            title = title,
-            body = body,
-            avatar = avatar,
-        )
+        _uiState.update { newUIState }
 
     }
 
@@ -113,10 +122,11 @@ class RankViewModel : ViewModel() {
         title = localizedString(R.string.rank_title, String.empty),
         body = localizedString(R.string.rank_body, String.empty),
         avatar = null,
+        actions = RankActions.COUNT_CLICKS,
     )
 
     companion object {
-        /*        private val RANK_0_RANGE = 0..1
+                private val RANK_0_RANGE = 0..1
                 private val RANK_1_RANGE = 2..3
                 private val RANK_2_RANGE = 4..5
                 private val RANK_3_RANGE = 6..7
@@ -125,18 +135,18 @@ class RankViewModel : ViewModel() {
                 private val RANK_6_RANGE = 12..13
                 private val RANK_7_RANGE = 14..15
                 private val RANK_8_RANGE = 16..17
-                private val RANK_9_RANGE = 18..Int.MAX_VALUE*/
+                private val RANK_9_RANGE = 18..Int.MAX_VALUE
 
-        private val RANK_0_RANGE = 0..9
-        private val RANK_1_RANGE = 10..19
-        private val RANK_2_RANGE = 20..34
-        private val RANK_3_RANGE = 35..54
-        private val RANK_4_RANGE = 55..79
-        private val RANK_5_RANGE = 80..109
-        private val RANK_6_RANGE = 110..144
-        private val RANK_7_RANGE = 145..184
-        private val RANK_8_RANGE = 185..229
-        private val RANK_9_RANGE = 230..Int.MAX_VALUE
+        /*       private val RANK_0_RANGE = 0..9
+               private val RANK_1_RANGE = 10..19
+               private val RANK_2_RANGE = 20..34
+               private val RANK_3_RANGE = 35..54
+               private val RANK_4_RANGE = 55..79
+               private val RANK_5_RANGE = 80..109
+               private val RANK_6_RANGE = 110..144
+               private val RANK_7_RANGE = 145..184
+               private val RANK_8_RANGE = 185..229
+               private val RANK_9_RANGE = 230..Int.MAX_VALUE*/
     }
 }
 
@@ -144,4 +154,10 @@ data class WelcomeUIState(
     val title: LocalizedString,
     val body: LocalizedString,
     @DrawableRes val avatar: Int?,
+    val actions: RankActions,
 )
+
+enum class RankActions {
+    COUNT_CLICKS,
+    UNIVERSE_DECISION
+}
